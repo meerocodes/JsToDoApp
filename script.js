dac5b2b902b6e545fe48a5d789bc65d4707dd278
meerocodes <amirest1997@gmail.com>
2023-02-15 15:45:17 -0500

    added pseudo code and logical comments

---
+++ script.js
@@ -0,0 +1,25 @@
+// Pseduo Code
+// 1) Save the values /DOM elements we will need to interact with
+    // ----
+    // let's query the DOM for the form element and save it within a variable
+
+    // Query the DOM for the input element and save it within a variable 
+
+// 2) attach an event listener to the Form element 
+    // pass in 2 arguments to the even listener method: 
+        // a) The event you are listening for
+        // b) the callback function which will hold the logic we wish run ONCE the event "is heard"( AKA occurs on the page)
+
+    // save the test value from the input within a variable
+
+    //IF the user submits a todo: (checl wether the value of the onput is NOT empty) then:
+        // create a li element
+            // add a FA icon to the element
+            // add the text from the todo that the user entered to the li
+
+
+        // append the li element to the ul element
+    // ELSE alert the user to please submit a calid to do
+
+// 3. Hwo do we tracj when the task is done
+
